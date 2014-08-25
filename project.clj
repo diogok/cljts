2b85cce35de423cae66a64ad6c8775fdd909ea82
Diogo Silva <diogo@diogok.net>
2014-08-25 18:59:10 -0300

    update dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cljts "0.3.0-SNAPSHOT"
+(defproject cljts "0.4.0"
   :description "Clojure wrapper of JTS, implements the Simple Feature Spec of Open Geospatial Consortium (OGC)."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.vividsolutions/jts "1.12"]
-                 [org.geotools/gt-geojson "8.2"]
-                 [midje "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.vividsolutions/jts "1.13"]
+                 [org.geotools/gt-geojson "11.1"]]
   :repositories [["opengeo" "http://repo.opengeo.org/"]]
-  :profiles {:dev {:plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}}
   :warn-on-reflection true)
