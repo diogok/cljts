0798c4a1512b920dac6a074c3f95181f0b5c68dc
Diogo Silva <diogo@diogok.net>
2016-05-24 20:06:41 -0300

    clojure version bump

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.diogok/cljts "0.4.6"
+(defproject org.clojars.diogok/cljts "0.4.7"
   :description "Clojure wrapper of JTS, implements the Simple Feature Spec of Open Geospatial Consortium (OGC)."
   :url "http://github.com/diogok/cljts"
   :license "Distributed under the Eclipse Public License, the same as Clojure."
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.vividsolutions/jts "1.13"]
                  [org.geotools/gt-geojson "13.2"]
                  [org.geotools/gt-geometry "13.2"]
