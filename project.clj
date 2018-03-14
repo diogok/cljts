1b5a4faee31287ec4d569cab08e8392b1840be82
Diogo Silva <diogo@diogok.net>
2018-03-14 18:59:31 -0300

    Upgrade geotools

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
-(defproject org.clojars.diogok/cljts "0.5.0"
+(defproject org.clojars.diogok/cljts "0.5.1"
   :description "Clojure wrapper of JTS, implements the Simple Feature Spec of Open Geospatial Consortium (OGC)."
   :url "http://github.com/diogok/cljts"
   :license "Distributed under the Eclipse Public License, the same as Clojure."
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.vividsolutions/jts "1.13"]
-                 [org.geotools/gt-geojson "14.4"]
-                 [org.geotools/gt-geometry "14.4"]
-                 [org.geotools/gt-epsg-hsql "14.4"]
-                 [org.geotools/gt-referencing "14.4"]]
+                 [org.geotools/gt-geojson "16.0"]
+                 [org.geotools/gt-geometry "16.0"]
+                 [org.geotools/gt-shapefile "16.0"]
+                 [org.geotools/gt-epsg-hsql "16.0"]
+                 [org.geotools/gt-referencing "16.0"]]
   :repositories [["osgeo" "http://download.osgeo.org/webdav/geotools/"]
                  ["clojars" {:sign-releases false}]]
   :profiles {:dev {:dependencies [[midje "1.8.2"]]
